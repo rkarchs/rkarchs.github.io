4d3efb238814982f21f26d3aae7aafc24874d309
rKarchs <>
2018-12-07 11:44:27 -0700

    ..

--- assignments/Term Final/scripts/closures.js
+++ assignments/Term Final/scripts/closures.js
@@ -1,35 +1,10 @@
-var bikeServices = new XMLHttpRequest();
-bikeServices.open('GET', 'https://gringuitolokito.github.io/cit230/assignments/Term%20Final/jsondata/services.json', true)
-bikeServices.send();
+var closures = new XMLHttpRequest();
+closures.open('GET', 'https://rkarchs.github.io/assignments/Term%20Final/jsondata/closures.json', true)
+closures.send();
 
-bikeServices.onload = function () {
-    var bS = JSON.parse(bikeServices.responseText);
-    console.log(bS);
+closures.onload = function () {
+    var dates = JSON.parse(closures.responseText);
+    console.log(dates);
 
-    document.getElementById('item1').innerHTML = bS.Services["0"].title;
-    document.getElementById('item2').innerHTML = "$" + bS.Services["0"].price;
-    document.getElementById('item3').innerHTML = bS.Services["1"].title;
-    document.getElementById('item4').innerHTML = "$" + bS.Services["1"].price;
-    document.getElementById('item5').innerHTML = bS.Services["2"].title;
-    document.getElementById('item6').innerHTML = "$" + bS.Services["2"].price;
-    document.getElementById('item7').innerHTML = bS.Services["3"].title;
-    document.getElementById('item8').innerHTML = "$" + bS.Services["3"].price;
-    document.getElementById('item9').innerHTML = bS.Services["4"].title;
-    document.getElementById('item10').innerHTML = "$" + bS.Services["4"].price;
-    document.getElementById('item11').innerHTML = bS.Services["5"].title;
-    document.getElementById('item12').innerHTML = "$" + bS.Services["5"].price + ' ' + bS.Services[5].details;
-    document.getElementById('item13').innerHTML = bS.Services["6"].title;
-    document.getElementById('item14').innerHTML = "$" + bS.Services["6"].price;
-    document.getElementById('item15').innerHTML = bS.Services["7"].title;
-    document.getElementById('item16').innerHTML = "$" + bS.Services["7"].price + ' ' + bS.Services[7].details;
-    document.getElementById('item17').innerHTML = bS.Services["8"].title;
-    document.getElementById('item18').innerHTML = "$" + bS.Services["8"].price + ' ' + bS.Services[8].details;
-    document.getElementById('item19').innerHTML = bS.Services["9"].title;
-    document.getElementById('item20').innerHTML = "$" + bS.Services["9"].price;
-    document.getElementById('item21').innerHTML = bS.Services["10"].title;
-    document.getElementById('item22').innerHTML = "$" + bS.Services["10"].price;
-    document.getElementById('item23').innerHTML = bS.Services["11"].title;
-    document.getElementById('item24').innerHTML = "$" + bS.Services["11"].price;
-    document.getElementById('item25').innerHTML = bS.Services["12"].title;
-    document.getElementById('item26').innerHTML = "$" + bS.Services["12"].price;
+//    document.getElementById('item1').innerHTML = ;
 };
