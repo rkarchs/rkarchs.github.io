f45427967903600458461bef7c31e6947677215a
upload <uploaded@git.edu>
2018-12-19 07:51:32 -0700

    ..

---
+++ assignments/termwebsiteproject/scripts/accord.js
@@ -0,0 +1,21 @@
+$(function () {
+    $("#accordion").accordion({
+        active: false,
+        heightStyle: "content",
+        collapsible: false
+    });
+});
+
+$(function () {
+    $("#sdtabs").tabs({
+        collapsible: true,
+        active: false,
+    });
+});
+
+$(function () {
+    $("#mtabs").tabs({
+        collapsible: true,
+        active: false,
+    });
+});
